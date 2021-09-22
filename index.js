3f5eb63dd0a61052898ab28e777e0626f2563f38
HaamzyTech <haamzy.hm@outlook.com>
2021-09-22 09:40:59 +0200

    added recapture

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var alertPlaceholder = document.getElementById('userAlert')
 
 function alert(message, type) {
     let wrapper = document.createElement('div')
-    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible mt-3" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
   
     alertPlaceholder.append(wrapper)
 }
@@ -15,6 +15,7 @@ const processForm = (e) => {
 
     grecaptcha.ready(function() {
         grecaptcha.execute('6Lckk38cAAAAANDwm_84BN8xIgMAVZ72NR4iomj4', {action: 'submit'}).then(function(token) {
+            formData.append('token',token)
             fetch('http://10.2.71.225:2000/external/registry/upload/form',{
                 method: 'POST',
                 body: formData,
